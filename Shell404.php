a273549f9a3dc9876e2449f2b675d642db5c5db0
Anon-404 <138273350+Anon-404@users.noreply.github.com>
2024-09-03 22:09:19 +0600

    Create Shell404.php

---
+++ Shell404.php
@@ -0,0 +1,9 @@
+<?php
+        $Team = "Anon404";
+        echo "Shell404 by $Team\n";
+
+        $Person = "William Steven";
+        echo "$Team by $Person\n\n";
+
+        echo "Coming soon............"
+?>
